62362d5c50f7bb6ac2041643f27b0885663af4c0
Sarah Vessels <cheshire137@gmail.com>
2017-06-25 21:10:33 -0500

    Add sign out button, style alerts

--- app/assets/javascripts/components/header.jsx
+++ app/assets/javascripts/components/header.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types'
 
+import LocalStorage from '../models/local-storage'
+
 const Header = function(props) {
   const { title } = props
+  const email = LocalStorage.get('email')
+  const authenticityToken = LocalStorage.get('authenticity-token')
+
   return (
     <div className="container">
       <nav className="nav">
@@ -18,6 +23,24 @@ const Header = function(props) {
             href="/accounts"
             className={`nav-item ${title === 'Accounts' ? 'is-active' : ''}`}
           >Accounts</a>
+        </div>
+        <div className="nav-right">
+          <span
+            className="nav-item"
+          >{email}</span>
+          <form
+            className="nav-item"
+            action="/users/sign_out"
+            method="post"
+          >
+            <input name="_method" type="hidden" value="delete" />
+            <input name="authenticity_token" type="hidden" value={authenticityToken} />
+            <button
+              className="button is-white"
+              type="submit"
+              onClick={() => LocalStorage.delete('email')}
+            >Sign out</button>
+          </form>
         </div>
       </nav>
     </div>
